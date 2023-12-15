bf748df4af39ee7f0e3896c2597bd166106e6e7a
Gosselin Noa <no.gosselin2006@gmail.com>
2023-12-15 22:41:21 +0100

    fixed api

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
 export async function GET (req : NextRequest){
 
         const params = req.nextUrl.searchParams
         const guessName = params.values().next().value
+
 
         const fetchPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${guessName}`,{cache : 'no-store'})
         const pokemon = await fetchPokemon.json()
@@ -12,9 +12,7 @@ export async function GET (req : NextRequest){
         const fetchSpecies = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${guessName}`,{cache:"no-cache"})
         const species = await fetchSpecies.json();
 
-
-
-        function getTypes(){
+        async function getTypes(){
                 if(pokemon.types.length == 1){
                         const type1 = pokemon.types[0].type.name;
                         const type2 = "aucun"
@@ -27,28 +25,37 @@ export async function GET (req : NextRequest){
         }
 
         async function getEvo(){
-                if(species.evolution_chain == null){
+                const fetchEvoChain = await fetch(species.evolution_chain.url,{cache:"no-cache"})
+                const EvoChain = await fetchEvoChain.json()
+                if(EvoChain.chain.species.name === pokemon.name){
                         return "1";
+                }else if((EvoChain.chain.evolves_to[0].species.name) === pokemon.name){
+                        return "2"
                 }else{
-                        const fetchEvoChain = await fetch(species.evolution_chain.url,{cache:"no-cache"})
-                        const EvoChain = await fetchEvoChain.json()
-
-                        if(EvoChain.chain.evolves_to.species.name == guessName){
-                                return "2"
-                        }else{
-                                return "3"
-                        }
+                        return "3"
                 }
         }
 
-        const img = pokemon.sprites.front_default;
-        const type1 = getTypes()[0];
-        const type2 = getTypes()[1];
-        const color = species.color.name;
-        const evo = getEvo();
-        const gen = species.generation.name;
-        const habitat = species.habitat.name;
+        async function getHabitat(){
+                if(species.habitat != null){
+                        return species.habitat.name;
+                }
+                else{
+                        return "aucun";
+                }
+                }
 
-        const result = NextResponse.json([img,type1,type2,color,evo,gen,habitat]);
+        const name = pokemon.name;
+        const img = pokemon.sprites.front_default;
+        
+        const [type1,type2] = await getTypes();
+
+        const color = species.color.name;
+        const evo = await getEvo();
+        const gen = species.generation.name;
+        const habitat = await getHabitat();
+
+
+        const result = NextResponse.json([name,img,type1,type2,color,evo,gen,habitat]);
         return result ;
 }
